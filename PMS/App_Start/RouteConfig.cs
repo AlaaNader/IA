e016d1e1bc47062965a1383b636d86d226d12cb6
AyaaaAbdelaziz <aya.mustafa.abdelaziz@gmail.com>
2019-04-23 13:11:52 +0200

    Add project files.

---
+++ PMS/App_Start/RouteConfig.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace PMS
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+            routes.MapRoute(
+              "customer", // Route name
+              "{controller}/{action}", // URL with parameters
+              new { controller = "Cust", action = "cut" } // Parameter defaults
+          );
+            routes.MapRoute(
+               "chome", // Route name
+               "{controller}/{action}", // URL with parameters
+               new { controller = "Home", action = "CHome" } // Parameter defaults
+           );
+            routes.MapRoute(
+             "signin", // Route name
+             "{controller}/{action}", // URL with parameters
+             new { controller = "Home", action = "signin" } // Parameter defaults
+         );
+            routes.MapRoute(
+         "Admin", // Route name
+         "{controller}/{action}", // URL with parameters
+         new { controller = "Admin", action = "Admin" } // Parameter defaults
+     );
+
+        }
+    }
+}
